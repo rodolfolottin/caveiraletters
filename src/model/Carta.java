d45b0d740f1f4d6dc18426984bbe6f006f5a8f8d
Rodolfo Lottin <rodolfolottin1@gmail.com>
2015-03-28 19:41:37 -0300

    Primeiro commit, implementação jogo, definição de classes

---
+++ src/model/Carta.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author rodolfolottin
+ */
+public abstract class Carta {
+
+    protected int indice;
+    protected String nome;
+    protected int valor;
+    protected Icon image;
+    protected int quantidade;
+    
+    public Carta (int indice, String nome, int valor, Icon image, int quantidade) {
+        this.indice = indice;
+        this.nome = nome;
+        this.valor = valor;
+        this.image = image;
+        this.quantidade = quantidade;
+    }
+    
+    public abstract boolean calcular(Carta adversaria);
+    
+    //public abstract void acaoCarta();
+
+    public int getIndice() {
+        return indice;
+    }
+
+    public void setIndice(int indice) {
+        this.indice = indice;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Icon getImage() {
+        return image;
+    }
+
+    public void setImage(Icon image) {
+        this.image = image;
+    }
+    
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
